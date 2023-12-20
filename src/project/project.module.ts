e0eac94c39271ece40534169aacd43e345623cda
ignacio <ignacio.farias.v@usach.cl>
2023-12-19 21:10:01 -0300

    feat(project): add project dto, controller and service

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ProjectController } from './controllers/project.controller';
+import { ProjectService } from './services/project.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProjectEntity } from './entities';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([ProjectEntity])],
+  controllers: [ProjectController],
+  providers: [ProjectService],
+})
 export class ProjectModule {}
---
+++ src/project/services/project.service.ts
@@ -0,0 +1,91 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { ProjectEntity } from '../entities';
+import { ErrorManager } from 'src/utils/error-manager';
+import { ProjectDTO, ProjectUpdateDTO } from '../dto/project.dto';
+
+@Injectable()
+export class ProjectService {
+  constructor(
+    @InjectRepository(ProjectEntity)
+    private readonly projectRepository: Repository<ProjectEntity>,
+  ) {}
+
+  public async createProject(body: ProjectDTO): Promise<ProjectEntity> {
+    try {
+      return await this.projectRepository.save(body);
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
+
+  public async findAllProjects(): Promise<ProjectEntity[]> {
+    try {
+      const projects: ProjectEntity[] = await this.projectRepository.find();
+      if (!projects.length) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'No projects found',
+        });
+      }
+      return projects;
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
+
+  public async findProjectById(id: string): Promise<ProjectEntity> {
+    try {
+      const project: ProjectEntity = await this.projectRepository.findOneBy({
+        id,
+      });
+      if (!project) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'Project not found',
+        });
+      }
+      return project;
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
+
+  public async updateProject(
+    id: string,
+    body: ProjectUpdateDTO,
+  ): Promise<UpdateResult | undefined> {
+    try {
+      const updatedProject: UpdateResult = await this.projectRepository.update(
+        id,
+        body,
+      );
+      if (updatedProject.affected === 0) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'Unable to update project',
+        });
+      }
+      return updatedProject;
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
+
+  public async deleteProject(id: string): Promise<DeleteResult | undefined> {
+    try {
+      const deletedProject: DeleteResult =
+        await this.projectRepository.delete(id);
+      if (deletedProject.affected === 0) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'Unable to delete project',
+        });
+      }
+      return deletedProject;
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
+}
